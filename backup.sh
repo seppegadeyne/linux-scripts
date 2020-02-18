7bbdd9d49d55a528bad235499e7e41e0dd3c73d7
Seppe Gadeyne <seppe@fushia.be>
2020-02-18 08:43:43 +0100

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+for user in `find /home -maxdepth 1 -type d`; do
+  if [[ $user != "." ]] && [[ -d "${user}/webapps" ]]; then
+    for web_application in `find "${user}/webapps" -maxdepth 1 -type d`; do
+      if [[ $web_application != "${user}/webapps" ]] && [[ ${user/\/home\//} != "arthur" ]]; then
+        cd ${web_application}
+        application_name=`echo ${web_application} | cut -d / -f 5`
+        echo "Working on ${application_name}..."
+        if [[ -e "${web_application}/wp-config.php" ]]; then
+          # Backup WordPress MySQL
+          sudo -u ${user/\/home\//} wp db export - | gzip > ./${application_name}_db_backup.sql.gz
+          cd /root/dropbox && ./dropbox_uploader.sh upload "${web_application}/${application_name}_db_backup.sql.gz" "/${user/\/home\//}/${application_name}/${application_name}_db_backup.sql.gz" >/dev/null
+          cd ${web_application} && rm ${application_name}_db_backup.sql.gz
+
+          # Backup WordPress files
+          cd "${user}/webapps" && tar -C "${user}/webapps" -czf "${application_name}_website_backup.tar.gz" "${application_name}/" >/dev/null
+          cd /root/dropbox && ./dropbox_uploader.sh upload "${user}/webapps/${application_name}_website_backup.tar.gz" "/${user/\/home\//}/${application_name}/${application_name}_website_backup.tar.gz" >/dev/null
+          cd "${user}/webapps" && rm ${application_name}_website_backup.tar.gz
+        else
+          # Backup website application files
+          cd "${user}/webapps" && tar -C "${user}/webapps" -czf "${application_name}_website_backup.tar.gz" "${application_name}/" >/dev/null
+          cd /root/dropbox && ./dropbox_uploader.sh upload "${user}/webapps/${application_name}_website_backup.tar.gz" "/${user/\/home\//}/${application_name}/${application_name}_website_backup.tar.gz" >/dev/null
+          cd "${user}/webapps" && rm ${application_name}_website_backup.tar.gz
+        fi
+      fi
+    done
+  fi
+done
