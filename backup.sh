9fd8848edf9f5c04c473a6d228a019c8bdf9a187
Seppe Gadeyne <seppe@fushia.be>
2020-02-18 09:12:02 +0100

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -6,6 +6,7 @@ for user in `find /home -maxdepth 1 -type d`; do
       if [[ $web_application != "${user}/webapps" ]] && [[ ${user/\/home\//} != "arthur" ]]; then
         cd ${web_application}
         application_name=`echo ${web_application} | cut -d / -f 5`
+        # echo "Working on ${application_name}..."
         if [[ -e "${web_application}/wp-config.php" ]]; then
           # Backup WordPress MySQL
           sudo -u ${user/\/home\//} wp db export - | gzip > ./${application_name}_db_backup.sql.gz
@@ -16,6 +17,11 @@ for user in `find /home -maxdepth 1 -type d`; do
           cd "${user}/webapps" && tar -C "${user}/webapps" -czf "${application_name}_website_backup.tar.gz" "${application_name}/" >/dev/null
           cd /root/dropbox && ./dropbox_uploader.sh upload "${user}/webapps/${application_name}_website_backup.tar.gz" "/${user/\/home\//}/${application_name}/${application_name}_website_backup.tar.gz" >/dev/null
           cd "${user}/webapps" && rm ${application_name}_website_backup.tar.gz
+
+          # Update WordPress plugins & core
+          cd ${web_application} && sudo -u ${user/\/home\//} wp plugin update --all --exclude=woocommerce --quiet &>/dev/null && sudo -u ${user/\/home\//} wp core update --quiet &>/dev/null
+          sudo -u ${user/\/home\//} wp transient delete --expired --quiet &>/dev/null
+          sudo -u ${user/\/home\//} wp db optimize --quiet &>/dev/null
         else
           # Backup website application files
           cd "${user}/webapps" && tar -C "${user}/webapps" -czf "${application_name}_website_backup.tar.gz" "${application_name}/" >/dev/null
