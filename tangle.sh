b39f62e103e302d80b4e31e5d2b67480d4380bf1
Seppe Gadeyne <seppe@fushia.be>
2021-06-08 20:19:04 +0200

    Update

--- tangle.sh
+++ tangle.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-INDEX_PAYLOAD=$(printf "hello-world-index" | xxd -ps)\
- && DATA_PAYLOAD=$(printf "some data" | xxd -ps)\
+INDEX_PAYLOAD=$(printf "tom-de-schutter" | xxd -ps)\
+ && DATA_PAYLOAD=$(printf "Wanneer terrasje?" | xxd -ps)\
  && PAYLOAD=$(echo -e "{\"payload\": {\"type\": 2,\"index\": \"$INDEX_PAYLOAD\", \"data\": \"$DATA_PAYLOAD\"}}")\
  && curl --request POST --header "Content-Type: application/json" --data "$PAYLOAD" --url https://api.lb-0.testnet.chrysalis2.com/api/v1/messages 
