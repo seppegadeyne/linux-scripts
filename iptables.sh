9a0e3bcf361ccb3e253b68232d4a3828f58875e5
Seppe Gadeyne <seppe@fushia.be>
2021-06-09 20:39:57 +0200

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -14,6 +14,9 @@ iptables -P FORWARD DROP
 iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
+## Excercise KdG
+iptables -A INPUT -p icmp -m limit -j LOG --log-prefix "IPTABLES ICMP: "
+
 ## Make sure NEW incoming tcp connections are SYN packets, otherwise drop them
 iptables -A INPUT -p tcp ! --syn -m state --state NEW -j DROP
 
@@ -23,34 +26,28 @@ iptables -A INPUT -p tcp --tcp-flags ALL FIN,PSH,URG -j DROP
 iptables -A INPUT -p tcp --tcp-flags ALL ALL -j DROP
 
 ## Drop incoming malformed XMAS packets
-iptables -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -m limit --limit 5/m --limit-burst 7 -j LOG --log-prefix "IPTABLES XMAS packets: "
 iptables -A INPUT -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP
 
 ## Drop all NULL packets
-iptables -A INPUT -p tcp --tcp-flags ALL NONE -m limit --limit 5/m --limit-burst 7 -j LOG --log-prefix "IPTABLES NULL packets: "
 iptables -A INPUT -p tcp --tcp-flags ALL NONE -j DROP
 iptables -A INPUT -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
 
 ## Drop FIN packet scans
-iptables -A INPUT -p tcp --tcp-flags FIN,ACK FIN -m limit --limit 5/m --limit-burst 7 -j LOG --log-prefix "IPTABLES FIN packets scan: "
 iptables -A INPUT -p tcp --tcp-flags FIN,ACK FIN -j DROP
 iptables -A INPUT -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
 
-## Log and get rid of broadcast / multicast and invalid
-iptables -A INPUT -m pkttype --pkt-type broadcast -j LOG --log-prefix "IPTABLES broadcast: "
+## Get rid of broadcast / multicast and invalid
 iptables -A INPUT -m pkttype --pkt-type broadcast -j DROP
-iptables -A INPUT -m pkttype --pkt-type multicast -j LOG --log-prefix "IPTABLES multicast: "
 iptables -A INPUT -m pkttype --pkt-type multicast -j DROP
-iptables -A INPUT -m state --state INVALID -j LOG --log-prefix "IPTABLES invalid: "
 iptables -A INPUT -m state --state INVALID -j DROP
 
 ## Reject connections above 30 from one source IP
 iptables -A INPUT -p tcp --syn --dport 80 -m connlimit --connlimit-above 100 --connlimit-mask 31 -j REJECT --reject-with tcp-reset
 iptables -A INPUT -p tcp --syn --dport 443 -m connlimit --connlimit-above 100 --connlimit-mask 31 -j REJECT --reject-with tcp-reset
 
-## Allow 300 new connections (packets) per second
-iptables -A INPUT -m state --state ESTABLISHED,RELATED -m limit --limit 300/second --limit-burst 320 -j ACCEPT
-iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+## Allow 1000 new connections (packets) per second
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -m limit --limit 1000/second --limit-burst 1250 -j ACCEPT
+iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j DROP
 
 ## Allow http and https
 iptables -A INPUT -p tcp --dport 80 -j ACCEPT
@@ -62,7 +59,7 @@ iptables -A OUTPUT -p tcp --dport 443 -m state --state ESTABLISHED -j ACCEPT
 iptables -A INPUT -p udp --dport 53 -j ACCEPT
 iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
 
-## Log all other and then drop them
+## Log all other connections
 iptables -A INPUT -j LOG --log-prefix "IPTABLES INPUT: "
 iptables -A OUTPUT -j LOG --log-prefix "IPTABLES OUTPUT: "
 
