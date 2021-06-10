9ef6c34fcfd20066b1776282a60a1c4c89e047a4
Seppe Gadeyne <seppe@fushia.be>
2021-06-10 11:14:24 +0200

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+## Set variables for iptables configuration
+IP_SERVER="161.97.160.205"
+# Your DNS servers you use: cat /etc/resolv.conf
+DNS_SERVER="127.0.0.53 161.97.189.51 161.97.189.52"
+
 ## Reset iptables
 iptables -F
 iptables -X
@@ -10,26 +15,9 @@ iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 iptables -P FORWARD DROP
 
-IP_SERVER="161.97.160.205"
-# Your DNS servers you use: cat /etc/resolv.conf
-DNS_SERVER="127.0.0.53 161.97.189.51 161.97.189.52"
-
 ## Allow everything on localhost
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
-
-for ip in $DNS_SERVER 
-do
-	echo "Allowing DNS lookups (tcp, udp port 53)"
-	iptables -A OUTPUT -p udp -d $ip --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-	iptables -A INPUT -p udp -s $ip --sport 53 -m state --state ESTABLISHED -j ACCEPT
-	iptables -A OUTPUT -p tcp -d $ip --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
-	iptables -A INPUT -p tcp -s $ip --sport 53 -m state --state ESTABLISHED -j ACCEPT
-done
-
-## Allow SSH
-iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
 
 ## Disable incoming ping requests
 iptables -A INPUT -p icmp --icmp-type 8 -s 0/0 -d $IP_SERVER -m state --state NEW,ESTABLISHED,RELATED -j DROP
@@ -69,29 +57,40 @@ iptables -A INPUT -p tcp --syn --dport 443 -m connlimit --connlimit-above 100 --
 
 ## Allow 1000 new connections (packets) per second
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -m limit --limit 1000/s --limit-burst 1250 -j ACCEPT
-# Should drop them here
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j DROP
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-## Redirect port 8080 to 80
-iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8080 -j REDIRECT --to-port 80
+## Allow SSH
+iptables -A INPUT -p tcp --dport 22 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 22 -m state --state ESTABLISHED -j ACCEPT
+
+## Allow DNS lookups
+for ip in $DNS_SERVER 
+do
+	iptables -A OUTPUT -p udp -d $ip --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+	iptables -A INPUT -p udp -s $ip --sport 53 -m state --state ESTABLISHED -j ACCEPT
+	iptables -A OUTPUT -p tcp -d $ip --dport 53 -m state --state NEW,ESTABLISHED -j ACCEPT
+	iptables -A INPUT -p tcp -s $ip --sport 53 -m state --state ESTABLISHED -j ACCEPT
+done
+
+## Redirect one port to another, idea for Node.js app server
+# iptables -t nat -A PREROUTING -i eth0 -p tcp --dport 8080 -j REDIRECT --to-port 80
 
 # Allowing new and established incoming connections to port 80, 443
-iptables -A INPUT  -p tcp -m multiport --dports 80,443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp -m multiport --dports 80,443 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A OUTPUT -p tcp -m multiport --sports 80,443 -m state --state ESTABLISHED -j ACCEPT
 
 # Allowing new and established outgoing connections to port 80, 443
-iptables -A OUTPUT  -p tcp -m multiport --dports 80,443 -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -m multiport --dports 80,443 -m state --state NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp -m multiport --sports 80,443 -m state --state ESTABLISHED -j ACCEPT
 
 # Allow outgoing connections to port 123 (ntp syncs)
 iptables -A OUTPUT -p udp --dport 123 -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT  -p udp --sport 123 -m state --state ESTABLISHED -j ACCEPT
+iptables -A INPUT -p udp --sport 123 -m state --state ESTABLISHED -j ACCEPT
 
-## Log all other connections
+## Log all other connections and drop them afterwards
 iptables -A INPUT -j LOG --log-prefix "IPTABLES INPUT: "
 iptables -A OUTPUT -j LOG --log-prefix "IPTABLES OUTPUT: "
-
-## Drop all other connections
 iptables -A INPUT -j DROP
 iptables -A OUTPUT -j DROP
 
